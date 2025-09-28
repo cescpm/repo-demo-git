2a5666b1d65c65dee076e603945c75b7a5ae9dcf
baieta <cpareramunoz@gmail.com>
2025-09-28 20:29:26 +0200

    Canvi de l'script i addició de les dades

--- src/script.py
+++ src/script.py
@@ -1,2 +1,11 @@
+def sum(a,b):
+    return a+b
+
+
 if __name__ == "__main__":
     print("Hello World")
+
+    import numpy as np
+
+    data = np.loadtxt('data/data.txt')
+    print(f"NumPy detected shape: {data.shape}")
