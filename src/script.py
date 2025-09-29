bab33a9080ddb8343fb9d2733372d0a411c52868
baieta <cpareramunoz@gmail.com>
2025-09-29 11:14:22 +0200

    Cropping the image

--- src/script.py
+++ src/script.py
@@ -11,6 +11,25 @@ if __name__ == "__main__":
     print(f"NumPy detected shape: {data.shape}")
 
     import PIL.Image
+    import ast
 
     img = PIL.Image.open('data/JoeBarTeam.jpg')
-    img.show()
+
+    users_choice = ''
+
+    while users_choice != 'Yes':
+
+        cropping_tuple = ast.literal_eval(input('Enter the coordinates where you want the image to be cut' \
+                                'Must fbe a 4-tuple: (left, upper, right, lower)'))
+
+        #(225,25,375,150)
+
+        cropped_img = img.crop(cropping_tuple)
+        cropped_img.show()
+
+        users_choice = input('Do you wish cropping the image this way? (Yes/No)')
+
+    
+
+
+
